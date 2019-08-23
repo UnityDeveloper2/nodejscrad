561050bebf9050fd907a602130a94552ed756803
UnityDeveloper2 <53303112+UnityDeveloper2@users.noreply.github.com>
2019-08-23 17:16:58 +0430

    add old code unity

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express=require('express');
+var app=express();
+var server=require('http').createServer(app);
+var io=require('socket.io').listen(server);
+var UserId=0;
 
-app.get('/', function(req, res){
-    res.response("kkkkkkk")
-});
 
-http.listen(3000, function(){
-    console.log('HTTP server started on port 3000');
+io.on('connection',function(socket){// A Listen To Recieve Data
+    var CurrentUser;
+    socket.on("Connect" , function (data) {
+        console.log("User Connected");
+        socket.emit("Connect",{name:data.name});//Send This To Client
+    });
+    socket.on("PLAY",function (data) {
+        UserId +=1;
+        CurrentUser={id:UserId,name:data.name,position:data.position};
+        console.log(CurrentUser.name);
+        socket.emit("PLAY",CurrentUser);
+        socket.broadcast.emit("OTHERPLAYERS",CurrentUser);
+    });
+    socket.on("MOVING",function (data) {
+        CurrentUser.position=data.position;
+        console.log("User :"+ CurrentUser.name + "Position:  " + CurrentUser.position);
+        socket.broadcast.emit("MOVING",CurrentUser);
+    });
 });

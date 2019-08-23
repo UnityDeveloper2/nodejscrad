4cf8640d659b8c3bf75e9fb1e1c871a72e6794ee
UnityDeveloper2 <53303112+UnityDeveloper2@users.noreply.github.com>
2019-08-23 18:22:07 +0430

    edi2

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
-var express=require('express');
-var app=express();
-var server=require('http').createServer(app);
-var io=require('socket.io').listen(server);
-var UserId=0;
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
 
-io.on('connection',function(socket){// A Listen To Recieve Data
-    var CurrentUser;
-    socket.on("Connect" , function (data) {
-        console.log("User Connected");
-        socket.emit("Connect",{name:data.name});//Send This To Client
-    });
-    socket.on("PLAY",function (data) {
-        UserId +=1;
-        CurrentUser={id:UserId,name:data.name,position:data.position};
-        console.log(CurrentUser.name);
-        socket.emit("PLAY",CurrentUser);
-        socket.broadcast.emit("OTHERPLAYERS",CurrentUser);
-    });
-    socket.on("MOVING",function (data) {
-        CurrentUser.position=data.position;
-        console.log("User :"+ CurrentUser.name + "Position:  " + CurrentUser.position);
-        socket.broadcast.emit("MOVING",CurrentUser);
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
     });
 });
-app.set('port',process.env.PORT || 80);
-server.listen(app.get('port'),function () {
-    console.log("Server Is On !");
-    setTimeout( function () {
-        console.log("Sever Is online")
-    }, 1000 );
+
+http.listen(80, function(){
+    console.log('listening on *:80');
 });
